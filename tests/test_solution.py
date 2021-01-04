205c312b0b341a98fcf5e998a7319000a5bda45b
Andy Ellis <andy_ellis@mac.com>
2021-01-05 06:22:26 +1000

    ADDED base test cases

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -8,4 +8,21 @@ import pytest
 	('1211', 10, 1)
 	])
 def test_solution(n, b, result):
+	""" Check that given solutions passs """
 	assert solution(n, b) == result
+
+
+@pytest.mark.parametrize('n, b', [
+	('1', 3),
+	('1234567890', 10).
+	(2, 2),
+	('123', '123'),
+	('ASA', 5),
+	('123', 1),
+	('123', 11),
+	('00123', 5)
+	])
+def test_language(n, b):
+	""" Validate that language passes requirements and edge cases """
+	with pytest.raises(ValueError):
+		solution(n, b)
